adac59ed93076d227bbe6b5cb9f048112d9da573
Otagis <113542968+Otagis@users.noreply.github.com>
2023-08-14 16:54:34 -0300

    upload
    14/08/2023
    
    piso randomizado

---
+++ Assets/Scripts/groundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] piso;
+    private Vector3 posicion = new Vector3(2.1604f, 4.045591f, 8.603987f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
