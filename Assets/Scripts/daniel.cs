af18456eb3afd92bf87cd7b773a91e1952938445
Otagis <113542968+Otagis@users.noreply.github.com>
2023-09-18 16:15:33 -0300

    Cambios en la camara del segundo juego

---
+++ Assets/Scripts/daniel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class daniel : MonoBehaviour
+{
+    public GameObject hola;
+    private int x;
+    private int z;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.H))
+        {
+            //x = Random.Range(-11, 11);
+            //z = Random.Range(-12, 12);
+
+            //Instantiate(hola, new Vector3(x, 10, z), Quaternion.identity);
+            Instantiate(hola, new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
