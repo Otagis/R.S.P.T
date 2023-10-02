32c9c02535674429082b20685ca99450a7eb5edf
Otagis <113542968+Otagis@users.noreply.github.com>
2023-10-02 14:49:15 -0300

    Pruebas de patrones y nuevo laser
    se creo un nuevo patron de asteroides para probar, se agrego un laser nuevo.

--- Assets/Scripts/CWACOA/daniel.cs
+++ Assets/Scripts/CWACOA/daniel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class daniel : MonoBehaviour
 {
     public GameObject hola;
+    private int patron;
     private float x;
     private float z;
     //Rigidbody rb;
@@ -18,15 +19,41 @@ public class daniel : MonoBehaviour
 
     void Update()
     {
+        if (patron == 0)
+        {
+            x = 11;
+            z = -11;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Instantiate(hola, new Vector3(x, 30f, z), Quaternion.identity);
+                x = x - 3;
+                z = z + 3;
+            }
+            patron = 1;
+        }
+        else if (patron == 1)
+        {
+
+        }
+        else if (patron == 2)
+        {
+
+        }
+        else if (patron == 3)
+        {
+
+        }
         if (Input.GetKey(KeyCode.H))
         {
-            x = Random.Range(-11f, 11f);
+            /*x = Random.Range(-11f, 11f);
             z = Random.Range(-12f, 12f);
 
             //rb.velocity = fallVelocity;
             Instantiate(hola, new Vector3(x, 30f, z), Quaternion.identity);
             
-            //Instantiate(hola, new Vector3(0, 1, 0), Quaternion.identity);
+            //Instantiate(hola, new Vector3(0, 1, 0), Quaternion.identity);*/
+            patron = 0;
         }
     }
 }
