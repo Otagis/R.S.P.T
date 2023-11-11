1072636dde610dd4dcbf55e1cd05cdb49d204589
nekosanti <110539710+nekosanti@users.noreply.github.com>
2023-11-11 18:58:07 -0300

    indicator script

---
+++ Assets/Scripts/DTSW/indicador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indicador : MonoBehaviour
+{
+    public Transform indicadorMovement;
+    perillaManager indicadorPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        indicadorPosition = GameObject.Find("Script").GetComponent<perillaManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        indicadorMovement.localEulerAngles = new Vector3(0, indicadorPosition.goal, 0);
+    }
+}
