92dc3b4a2569569fd840f6fd3b96e2c9101f9e05
Otagis <113542968+Otagis@users.noreply.github.com>
2023-11-13 12:10:03 -0300

    mayor update
    Se subio el 3er juego con todas las perillas funcionales. Falta el modelo de radio y lo ultimo es el game manager.

---
+++ Assets/Scripts/DTSW/indicador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indicador : MonoBehaviour
+{
+    public Transform indicadorMovement1;
+    public Transform indicadorMovement2;
+    public Transform indicadorMovement3;
+    perillaManager indicadorPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        indicadorPosition = GameObject.Find("Script").GetComponent<perillaManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        indicadorMovement1.localEulerAngles = new Vector3(0, indicadorPosition.goal1, 0);
+        indicadorMovement2.localEulerAngles = new Vector3(0, indicadorPosition.goal2, 0);
+        indicadorMovement3.localEulerAngles = new Vector3(0, indicadorPosition.goal3, 0);
+    }
+}
---
+++ Assets/Scripts/DTSW/perillaManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using Random = UnityEngine.Random;
+
+public class perillaManager : MonoBehaviour
+{
+    SerialPort serialPort = new SerialPort("COM6", 9600);
+    public Transform perillaMovement1;
+    public Transform perillaMovement2;
+    public Transform perillaMovement3;
+    public bool isComplete1;
+    public bool isComplete2;
+    public bool isComplete3;
+    public int goal1;
+    public int goal2;
+    public int goal3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialPort.Open();
+        serialPort.ReadTimeout = 1;
+        isComplete1 = false;
+        isComplete2 = false;
+        isComplete3 = false;
+        goal1 = Random.Range(0, 360);
+        goal2 = Random.Range(0, 360);
+        goal3 = Random.Range(0, 360);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if (serialPort.IsOpen)
+        {
+            string value = serialPort.ReadLine();
+            string[] pos = value.Split(',');
+            Debug.Log(value);
+
+            int perilla1 = Convert.ToInt32(pos[4]);
+            int perilla2 = Convert.ToInt32(pos[5]);
+            int perilla3 = Convert.ToInt32(pos[6]);
+
+            if (perilla1 == goal1)
+            {
+                isComplete1 = true;
+            }
+
+            if (isComplete1 == false)
+            {
+                perillaMovement1.localEulerAngles = new Vector3(0, perilla1, 0);
+            }
+            else if (isComplete1 == true)
+            {
+                perillaMovement1.localEulerAngles = new Vector3(0, goal1, 0);
+            }
+            
+            if (perilla2 == goal2)
+            {
+                isComplete2 = true;
+            }
+
+            if (isComplete2 == false)
+            {
+                perillaMovement2.localEulerAngles = new Vector3(0, perilla2, 0);
+            }
+            else if (isComplete2 == true)
+            {
+                perillaMovement2.localEulerAngles = new Vector3(0, goal2, 0);
+            }
+
+            if (perilla3 == goal3)
+            {
+                isComplete3 = true;
+            }
+
+            if (isComplete3 == false)
+            {
+                perillaMovement3.localEulerAngles = new Vector3(0, perilla3, 0);
+            }
+            else if (isComplete3 == true)
+            {
+                perillaMovement3.localEulerAngles = new Vector3(0, goal3, 0);
+            }
+        }
+        
+    }
+
+}
