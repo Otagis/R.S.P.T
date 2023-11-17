191feefd162b0f03363ad381517e7f6e50b3cfa4
Otagis <113542968+Otagis@users.noreply.github.com>
2023-11-17 15:05:55 -0300

    No importante

---
+++ Assets/Scripts/GameManagment/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static SceneLoader instance { get; private set; }
+    public int sceneNum;
+
+        
+    public void LoadNextScene()
+    {
+        sceneNum += 1;
+        SceneManager.LoadScene(sceneNum);
+        if (sceneNum == 4)
+        {
+            sceneNum = 1;
+        }
+    }
+}
